5abc770db797baa5a3ce1d85bc78477bbeadcd1f
dans46 <62874416+dans46@users.noreply.github.com>
2020-03-30 10:29:10 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+$(document).ready(function(){
+
+	$("#pr").hover(function(){
+    $("#bg").css('background', 'url("img/priroda.jpeg")').css('background-size', 'cover') .css('background-position', 'center center');
+  });
+
+  	$("#arch").hover(function(){
+    $("#bg").css('background', 'url("img/architecture.jpeg")').css('background-size', 'cover') .css('background-position', 'center center');
+  });
+
+  $("#peop").hover(function(){
+    $("#bg").css('background', 'url("img/people.jpg")').css('background-size', 'cover') .css('background-position', 'center center');
+  });
+
+  $("#food").hover(function(){
+    $("#bg").css('background', 'url("img/food.jpg")').css('background-size', 'cover') .css('background-position', 'center center');
+  });
+
+  $("#cars").hover(function(){
+    $("#bg").css('background', 'url("img/cars.jpeg")').css('background-size', 'cover') .css('background-position', 'center center');
+  });
+
+
+});
